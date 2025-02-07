44f68c1a696ca2f1f1b5534ad01059dae3fdad5a
Eduardo Cruz <50181252+GenomaVoid@users.noreply.github.com>
2025-02-06 18:45:01 -0600

    ReactBaseV2
    Nueva base de react ahora con vite + react en java script

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { NavBar } from './components/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+      <p>hola mundo</p>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
